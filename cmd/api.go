d22b34d1c2e350a4833ed20f92ae8f056b9f9a6e
Tnozone <maxnocquet@gmail.com>
2026-03-17 16:41:43 +0100

    middleware

--- cmd/api.go
+++ cmd/api.go
@@ -2,6 +2,7 @@ package main
 
 import {
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -14,11 +15,24 @@ type application struct {
 //mount
 func (app *application) mount() http.Handler {
 	r := chi.NewRouter()
+
+	// A good base middleware stack
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	// Set a timeout value on the request context (ctx), that will signal
+	// through ctx.Done() that the request has timed out and further
+	// processing should be stopped.
+	r.Use(middleware.Timeout(60 * time.Second))
+
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome"))
+		w.Write([]byte("hi"))
 	})
-	http.ListenAndServe(":3000", r)
+	// http.ListenAndServe(":3000", r)
+
+	return r
 }
 
 //run
