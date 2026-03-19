9ee39e89968635c5b1bbc8ba011b75348c555f79
Tnozone <maxnocquet@gmail.com>
2026-03-19 22:59:05 +0100

    mount and error handling

--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,15 @@ package main
 func main() {
 	cfg := config{
 		addr: ":8080",
-		db: dbConfig{},
+		db:   dbConfig{},
 	}
 
 	api := application{
 		config: cfg,
+	}
+
+	if err := api.run(api.mount()); err != nil {
+		log.Printf("server has failed to start, err %s", err)
+		os.Exit(1)
 	}
 }
