eadee256d40686e921323a5139ccdd2e9bc103b5
Tnozone <maxnocquet@gmail.com>
2026-04-10 22:26:22 +0200

    connection

--- cmd/main.go
+++ cmd/main.go
@@ -1,34 +1,40 @@
 package main
 
 import (
+	"context"
 	"log/slog"
 	"os"
 
+	"github.com/Tnozone/ecom/internal/env"
 	"github.com/jackc/pgx/v5"
 )
 
 func main() {
+	ctx := context.Background()
+
 	cfg := config{
 		addr: ":8080",
 		db: dbConfig{
-			dsn: "host=localhost user=postgres password=postgres dbname=ecom sslmode=disabled",
+			dsn: env.Getstring("GOOSE_DBSTRING", "host=localhost user=postgres password=postgres dbname=ecom sslmode=disabled"),
 		},
-	}
-
-	//Database
-	conn, err := pgx.Connect(ctx, cfg.db.dsn)
-	if err != nil {
-		return err
-	}
-	defer conn.Close(ctx)
-
-	api := application{
-		config: cfg,
 	}
 
 	// Logger
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 	slog.SetDefault(logger)
+
+	//Database
+	conn, err := pgx.Connect(ctx, cfg.db.dsn)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close(ctx)
+
+	logger.Info("connected to database", "dsn", cfg.db.dsn)
+
+	api := application{
+		config: cfg,
+	}
 
 	if err := api.run(api.mount()); err != nil {
 		slog.Error("server failed to start", "error", err)
